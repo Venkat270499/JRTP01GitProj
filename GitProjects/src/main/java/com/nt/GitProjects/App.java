9166ea39a871ef51378f0da863555c8baf8cc2ca
Sai <venkat>
2024-05-01 14:00:32 +0530

    Messing with common commit

--- GitProjects/src/main/java/com/nt/GitProjects/App.java
+++ GitProjects/src/main/java/com/nt/GitProjects/App.java
@@ -1,4 +1,4 @@
-package com.nt.GitProjects;
+ package com.nt.GitProjects;
 
 /**
  * Hello world!
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hey Venkat This is my first commit from Dev1" );
+        
+        System.out.println("Let me mess it up with some common commit without pull");
     }
 }
