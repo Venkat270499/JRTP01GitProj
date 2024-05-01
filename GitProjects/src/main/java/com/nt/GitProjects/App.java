4abbf71deaef9893cff2e8e61089761f520ff986
Sai Venkat <venkat>
2024-05-01 14:01:53 +0530

    Commit form dev1 venkat

--- GitProjects/src/main/java/com/nt/GitProjects/App.java
+++ GitProjects/src/main/java/com/nt/GitProjects/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hey Venkat This is my first commit from Dev1" );
+        
+        System.out.println("Let me commit first");
     }
 }
